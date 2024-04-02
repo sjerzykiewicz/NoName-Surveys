21b3e96cb3965c9c12e9dcad0ad663d5e7cfbe75
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2024-04-02 21:39:46 +0200

    NONAME-12 added api middleware for CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:5173", "localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
-    return {"Hello": "World"}
+    return {"message": "Hello World!"}

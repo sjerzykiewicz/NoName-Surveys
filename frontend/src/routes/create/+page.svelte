41394df31a94be40ba4c5264e45dce90ac023322
K4RP4T <kactop@st.amu.edu.pl>
2024-04-14 21:16:31 +0200

    NONAME-6 removed logs | put components in $lib

--- frontend/src/routes/create/+page.svelte
+++ frontend/src/routes/create/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import Header from '../Header.svelte';
-	import Content from '../Content.svelte';
-	import Footer from '../Footer.svelte';
-	import SurveyTitle from './components/SurveyTitle.svelte';
-	import Questions from './components/Questions.svelte';
-	import FooterButtons from './components/FooterButtons.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Content from '$lib/components/Content.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import SurveyTitle from '$lib/components/SurveyTitle.svelte';
+	import Questions from '$lib/components/Questions.svelte';
+	import FooterButtons from '$lib/components/FooterButtons.svelte';
 </script>
 
 <Header>

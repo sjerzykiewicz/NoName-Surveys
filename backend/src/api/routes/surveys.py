3a4ded458a067dac1e18c71a7cd6af7be59cdc85
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-04-24 16:05:40 +0200

    NONAME-35 json format specification | survey creation endpoint

--- backend/src/api/routes/surveys.py
+++ backend/src/api/routes/surveys.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
 
 router = APIRouter()
 
@@ -9,3 +10,73 @@ router = APIRouter()
 )
 def list_surveys():
     return {"message": "List all surveys"}
+
+
+class Question(BaseModel):
+    required: bool
+    question: str
+
+
+class SingleQuestionBody(BaseModel):
+    choices: list[str] = Field(
+        min_length=2, description="SingleQuestion must have at least 2 choices"
+    )
+
+
+class SingleQuestion(Question):
+    single: SingleQuestionBody
+
+
+class MultiQuestionBody(BaseModel):
+    choices: list[str] = Field(
+        min_length=2, description="MultiQuestion must have at least 2 choices"
+    )
+
+
+class MultiQuestion(Question):
+    multi: MultiQuestionBody
+
+
+class ScaleQuestionBody(BaseModel):
+    pass
+
+
+class ScaleQuestion(Question):
+    scale: ScaleQuestionBody
+
+
+class SliderQuestionBody(BaseModel):
+    min: float
+    max: float
+
+
+class SliderQuestion(Question):
+    slider: SliderQuestionBody
+
+
+class RankQuestionBody(BaseModel):
+    choices: list[str] = Field(
+        min_length=2, description="RankQuestion must have at least 2 choices"
+    )
+
+
+class RankQuestion(Question):
+    rank: RankQuestionBody
+
+
+class TextQuestionBody(BaseModel):
+    details: str
+
+
+class TextQuestion(Question):
+    text: TextQuestionBody
+
+
+class Survey(BaseModel):
+    title: str
+    questions: list[Question] = []
+
+
+@router.post("/create", response_description="Create a new survey")
+async def create_survey(survey_create: Survey):
+    return {"message": "OK"}

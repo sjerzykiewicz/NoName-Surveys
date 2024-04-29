5e2a5568b871823e16afb002b98dc8a0fb231001
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-04-29 19:15:15 +0200

    NONAME-11 removed useless file

--- frontend/src/routes/fill/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Actions } from './$types';
-
-export const actions: Actions = {
-	default: async ({ request }) => {
-		request.formData();
-		// TODO signature, send to backend
-	}
-};

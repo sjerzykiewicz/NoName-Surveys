7158ca356d9771b1a09bbf6d5e2accb894467bfa
yanvoi <janwojciechowski@op.pl>
2024-05-03 16:11:30 +0200

    get rid of redundant comma

--- backend/src/db/base.py
+++ backend/src/db/base.py
@@ -2,9 +2,7 @@ from sqlmodel import Session, SQLModel, create_engine
 
 # this is to be changed later on in the development
 engine = create_engine(
-    "sqlite://",
-    echo=True,
-    connect_args={"check_same_thread": False},
+    "sqlite://", echo=True, connect_args={"check_same_thread": False}
 )
 
 

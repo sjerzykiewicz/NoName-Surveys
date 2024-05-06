329c59c459a06438c11abee89caf27e670f07d4e
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-05-06 13:43:05 +0200

    NONAME-57 implemented answer submitting

--- frontend/src/lib/server/database.ts
+++ frontend/src/lib/server/database.ts
@@ -1,5 +1,6 @@
-import type Survey from '$lib/entities/Survey';
-import type SurveyCreateInfo from '$lib/entities/SurveyCreateInfo';
+import type Survey from '$lib/entities/surveys/Survey';
+import type { SurveyAnswer } from '$lib/entities/surveys/SurveyAnswer';
+import type SurveyCreateInfo from '$lib/entities/surveys/SurveyCreateInfo';
 import { error } from '@sveltejs/kit';
 
 const host = 'http://localhost:8000';
@@ -36,4 +37,20 @@ export const getSurveyByCode = async (survey_code: string) => {
 
 	const survey_structure: Survey = (await response.json()).survey_structure;
 	return { survey_structure };
+};
+
+export const saveAnswer = async (answer: SurveyAnswer) => {
+	const response = await fetch(`${host}/answers/fill`, {
+		method: 'POST',
+		body: JSON.stringify(answer),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	if (!response.ok) {
+		throw error(response.status, response.statusText);
+	}
+
+	return (await response.json()).message;
 };

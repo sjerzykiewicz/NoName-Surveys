f8cae57338c52f3f0bced6df621636aa18169e28
K4RP4T <kactop@st.amu.edu.pl>
2024-05-12 01:56:58 +0200

    NONAME-79 added QR code and made some changes to codeview page

--- frontend/src/routes/[code]/view/+page.svelte
+++ frontend/src/routes/[code]/view/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Content from '$lib/components/Content.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -15,17 +19,33 @@
 	}
 </script>
 
-<div class="content">
-	<h2>Survey created successfully.</h2>
-	<h2>Access code:</h2>
-	<h1>{data.code}</h1>
-	<button class="save" on:click={copyCode}
-		><i class="material-symbols-rounded">content_copy</i>Copy</button
+<Header>
+	<div class="title">Survey created successfully.</div>
+</Header>
+
+<Content>
+	<div class="content">
+		<h2>Access code:</h2>
+		<h1>{data.code}</h1>
+		<a href="/{data.code}/fill" title="Go to fill page">
+			<img
+				src="https://api.qrserver.com/v1/create-qr-code/?data={$page.url.origin}/{data.code}/fill"
+				alt="QR Code"
+			/>
+		</a>
+	</div>
+</Content>
+
+<Footer>
+	<button title="Copy code" class="save" on:click={copyCode}
+		><i class="material-symbols-rounded">content_copy</i>
+		{#if isCopied}
+			Copied!
+		{:else}
+			Copy
+		{/if}</button
 	>
-	{#if isCopied}
-		<h2>Copied!</h2>
-	{/if}
-</div>
+</Footer>
 
 <style>
 	.content {
@@ -36,20 +56,20 @@
 	}
 
 	h2 {
+		margin: 0em;
 		font-size: 3em;
 		cursor: default;
 	}
 
 	h1 {
 		margin: 0em;
+		margin-bottom: 0.25em;
 		font-size: 7em;
 		cursor: text;
 	}
 
-	.save {
-		margin: auto;
-		margin-top: 1em;
-		font-size: 2em;
+	img {
+		width: 50%;
 	}
 
 	@media screen and (max-width: 767px) {

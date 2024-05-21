660066395913753c0e0d4b6867454192dbc54bfd
yanvoi <janwojciechowski@op.pl>
2024-05-21 20:54:32 +0200

    allow passing db connection info and update poetry dependencies

--- backend/src/db/base.py
+++ backend/src/db/base.py
@@ -2,12 +2,42 @@ from sqlmodel import Session, SQLModel, create_engine
 
 from config import settings
 
-# this is to be changed later on in the development
-engine = create_engine(
-    "sqlite://",
-    echo=(settings.environment_type == "dev"),
-    connect_args={"check_same_thread": False},
-)
+
+def db_connection_is_provided():
+    return (
+        settings.db_type
+        and settings.db_user
+        and settings.db_password
+        and settings.db_host
+        and settings.db_name
+    )
+
+
+def create_url():
+    db_type = settings.db_type
+    db_dialect = "+" + settings.db_dialect if settings.db_dialect else ""
+    db_user = settings.db_user
+    db_password = settings.db_password
+    db_host = settings.db_host
+    db_port = ":" + settings.db_port if settings.db_port else ""
+    db_name = settings.db_name
+
+    return f"{db_type}{db_dialect}://{db_user}:{db_password}@{db_host}{db_port}/{db_name}"
+
+
+def create_db_engine():
+    return (
+        create_engine(create_url(), echo=(settings.environment_type == "dev"))
+        if db_connection_is_provided()
+        else create_engine(
+            "sqlite://",
+            echo=(settings.environment_type == "dev"),
+            connect_args={"check_same_thread": False},
+        )
+    )
+
+
+engine = create_db_engine()
 
 
 def init_db():

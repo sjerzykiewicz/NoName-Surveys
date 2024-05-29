a916bf9879c56484bfc4999cfc2813e842e0439c
K4RP4T <kactop@st.amu.edu.pl>
2024-05-29 18:33:07 +0200

    NONAME-38 restored previous component layout

--- frontend/src/routes/create/+page.svelte
+++ frontend/src/routes/create/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Content from '$lib/components/Content.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import SurveyTitle from '$lib/components/create-page/SurveyTitle.svelte';
+	import SurveyTitlePreview from '$lib/components/create-page/preview/SurveyTitlePreview.svelte';
 	import SurveyForm from '$lib/components/create-page/SurveyForm.svelte';
+	import FooterButtons from '$lib/components/create-page/FooterButtons.svelte';
+
+	export let titleError: boolean;
+	export let isPreview: boolean;
 </script>
 
-<SurveyForm />
+<Header>
+	{#if isPreview}
+		<SurveyTitlePreview />
+	{:else}
+		<SurveyTitle {titleError} />
+	{/if}
+</Header>
+<Content>
+	<SurveyForm {isPreview} />
+</Content>
+<Footer>
+	<FooterButtons bind:titleError bind:isPreview />
+</Footer>

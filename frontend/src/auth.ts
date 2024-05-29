985d5613785336bea93dbc6f17e1f48e7e485d31
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2024-05-29 11:37:06 +0200

    fixed docker

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,12 +1,12 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const { handle } = SvelteKitAuth({
 	providers: [
 		Google({
-			clientId: GOOGLE_CLIENT_ID,
-			clientSecret: GOOGLE_CLIENT_SECRET
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET
 		})
 	],
 	trustHost: true,

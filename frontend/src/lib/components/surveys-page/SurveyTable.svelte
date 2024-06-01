1e95f402f438a241ead304a1547277a2702a84b2
K4RP4T <kactop@st.amu.edu.pl>
2024-06-01 14:49:21 +0200

    NONAME-91 added easy code copy

--- frontend/src/lib/components/surveys-page/SurveyTable.svelte
+++ frontend/src/lib/components/surveys-page/SurveyTable.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { copyCode } from '$lib/utils/copyCode';
+
+	let copiedIndex: number;
 
 	export let survey_list: {
 		title: string;
@@ -15,14 +18,21 @@
 		<th title="Access code" id="code-header">Code</th>
 		<th title="Creation date" id="date-header">Date</th>
 	</tr>
-	{#each survey_list.toReversed() as entry}
+	{#each survey_list.toReversed() as entry, entryIndex}
 		<tr>
 			<td
 				title="Click to view summary"
 				class="title-entry"
 				on:click={() => goto('/' + entry.code + '/summary')}>{entry.title}</td
 			>
-			<td title="Access code" class="code-entry">{entry.code}</td>
+			<td
+				title={copiedIndex === entryIndex ? 'Copied!' : 'Click to copy code'}
+				class="code-entry"
+				on:click={() => {
+					copyCode(entry.code);
+					copiedIndex = entryIndex;
+				}}>{entry.code}</td
+			>
 			<td title="Creation date" class="date-entry">{entry.creation_date}</td>
 		</tr>
 	{/each}
@@ -31,7 +41,10 @@
 <style>
 	table {
 		width: 100%;
-		border-collapse: collapse;
+		border: 1px solid var(--border-color);
+		border-collapse: separate;
+		border-spacing: 0;
+		border-radius: 5px;
 		box-shadow: 0px 4px 4px var(--shadow-color);
 		font-size: 1.25em;
 		color: var(--text-color);
@@ -39,7 +52,6 @@
 
 	th,
 	td {
-		border: 1px solid var(--border-color);
 		padding: 0.25em;
 		text-align: left;
 		text-shadow: 0px 4px 4px var(--shadow-color);
@@ -56,32 +68,59 @@
 		overflow-wrap: anywhere;
 	}
 
-	tr:nth-child(even) {
+	tr:nth-child(2n + 1) td {
+		background-color: var(--primary-dark-color);
+	}
+
+	tr:nth-child(2n + 2) td {
 		background-color: var(--primary-color);
 	}
 
-	tr:nth-child(odd) {
-		background-color: var(--primary-dark-color);
+	tr:first-of-type {
+		border-radius: 4px 4px 0px 0px;
+	}
+
+	tr:last-of-type {
+		border-radius: 0px 0px 4px 4px;
+	}
+
+	th:first-of-type {
+		border-top-left-radius: 4px;
+	}
+
+	th:last-of-type {
+		border-top-right-radius: 4px;
+	}
+
+	tr:last-of-type td:first-of-type {
+		border-bottom-left-radius: 4px;
+	}
+
+	tr:last-of-type td:last-of-type {
+		border-bottom-right-radius: 4px;
 	}
 
 	#title-header {
 		width: 70%;
 	}
 
-	.title-entry {
+	.title-entry,
+	.code-entry {
 		cursor: pointer;
 	}
 
-	.title-entry:hover {
+	.title-entry:hover,
+	.code-entry:hover {
 		background-color: var(--secondary-color);
+	}
+
+	.title-entry:active,
+	.code-entry:active {
+		background-color: var(--border-color);
 	}
 
 	#code-header {
 		width: 12%;
-	}
-
-	.code-entry {
-		cursor: text;
 	}
 
 	#date-header {

4870ff33e789cc4d0550cf1fc84fdc54fb25e7f7
K4RP4T <kactop@st.amu.edu.pl>
2024-06-01 15:36:54 +0200

    NONAME-91 optimized tables CSS

--- frontend/src/lib/components/drafts-page/DraftsTable.svelte
+++ frontend/src/lib/components/drafts-page/DraftsTable.svelte
@@ -167,96 +167,15 @@
 </table>
 
 <style>
-	table {
-		width: 100%;
-		border: 1px solid var(--border-color);
-		border-collapse: separate;
-		border-spacing: 0;
-		border-radius: 5px;
-		box-shadow: 0px 4px 4px var(--shadow-color);
-		font-size: 1.25em;
-		color: var(--text-color);
-	}
-
-	th,
-	td {
-		padding: 0.25em;
-		text-align: left;
-		text-shadow: 0px 4px 4px var(--shadow-color);
-	}
-
-	th {
-		background-color: var(--secondary-dark-color);
-		font-weight: bold;
-		cursor: default;
-		overflow-wrap: break-word;
-	}
-
-	td {
-		overflow-wrap: anywhere;
-	}
-
-	tr:nth-child(2n + 1) td {
-		background-color: var(--primary-dark-color);
-	}
-
-	tr:nth-child(2n + 2) td {
-		background-color: var(--primary-color);
-	}
-
-	tr:first-of-type {
-		border-radius: 4px 4px 0px 0px;
-	}
-
-	tr:last-of-type {
-		border-radius: 0px 0px 4px 4px;
-	}
-
-	th:first-of-type {
-		border-top-left-radius: 4px;
-	}
-
-	th:last-of-type {
-		border-top-right-radius: 4px;
-	}
-
-	tr:last-of-type td:first-of-type {
-		border-bottom-left-radius: 4px;
-	}
-
-	tr:last-of-type td:last-of-type {
-		border-bottom-right-radius: 4px;
-	}
-
 	#title-header {
 		width: 82%;
-	}
-
-	.title-entry {
-		cursor: pointer;
-	}
-
-	.title-entry:hover {
-		background-color: var(--secondary-color);
-	}
-
-	.title-entry:active {
-		background-color: var(--border-color);
 	}
 
 	#date-header {
 		width: 18%;
 	}
 
-	.date-entry {
-		cursor: default;
-	}
-
 	@media screen and (max-width: 767px) {
-		table {
-			font-size: 0.8em;
-		}
-
 		#title-header {
 			width: 74%;
 		}

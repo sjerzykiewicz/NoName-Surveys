e43e51201287f6477018321a72864f98744441ee
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2024-06-04 21:24:38 +0200

    NONAME-106: click me removed

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,24 +8,7 @@
 	$title = '';
 	$questions = [];
 	$answers = [];
-
-	import init, { get_keypair } from 'wasm';
-	import { onMount } from 'svelte';
-
-	onMount(async () => {
-		await init();
-	});
 </script>
 
 <WelcomingBanner />
 <SurveyCodeForm {form} />
-
-<div>
-	<button
-		on:click={() => {
-			let keypair = get_keypair();
-			console.log(keypair.get_public_key());
-			console.log(keypair.get_private_key());
-		}}>Click Me</button
-	>
-</div>

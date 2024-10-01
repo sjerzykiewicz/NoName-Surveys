b4cad669a5e0d4d96be57583a03b398dd705a295
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2024-10-01 08:37:31 +0200

    added github provider

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,5 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
+import GitHub from '@auth/sveltekit/providers/github';
 import { env } from '$env/dynamic/private';
 import * as db from '$lib/server/database';
 
@@ -8,7 +9,8 @@ export const { handle } = SvelteKitAuth({
 		Google({
 			clientId: env.GOOGLE_CLIENT_ID,
 			clientSecret: env.GOOGLE_CLIENT_SECRET
-		})
+		}),
+		GitHub
 	],
 	trustHost: true,
 	secret: env.AUTH_SECRET,

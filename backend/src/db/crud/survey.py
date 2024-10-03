7de7fbc2d10d701c6eab38ad72202633d650c6a7
yanvoi <janwojciechowski@op.pl>
2024-10-03 17:32:53 +0200

    NONAME-143 do not include deleted surveys in response

--- backend/src/db/crud/survey.py
+++ backend/src/db/crud/survey.py
@@ -94,7 +94,7 @@ def get_all_surveys_user_can_view(
         for access in survey_accesses
     ]
 
-    return [(survey, survey.creator_id == user_id) for survey in surveys]
+    return [(survey, survey.creator_id == user_id) for survey in surveys if survey]
 
 
 def get_all_users_with_access_to_survey(

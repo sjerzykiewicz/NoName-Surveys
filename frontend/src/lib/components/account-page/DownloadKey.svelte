d394754f83d0b720f2e7c6aa264896bade283e4f
K4RP4T <kactop@st.amu.edu.pl>
2024-10-06 23:00:55 +0200

    NONAME-153 modal when generating keys

--- frontend/src/lib/components/account-page/DownloadKey.svelte
+++ frontend/src/lib/components/account-page/DownloadKey.svelte
@@ -1,63 +1,18 @@
 <script lang="ts">
-	import type { PageServerData } from '../../../routes/account/$types';
-	import init, { get_keypair } from 'wasm';
-	import { onMount } from 'svelte';
-
-	export let data: PageServerData;
+	export let isModalHidden: boolean = true;
 
 	let innerWidth: number;
-
-	onMount(async () => {
-		await init();
-	});
-
-	function download(filename: string, text: string) {
-		var element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-		element.setAttribute('download', filename);
-
-		element.style.display = 'none';
-		document.body.appendChild(element);
-
-		element.click();
-
-		document.body.removeChild(element);
-	}
-
-	async function generateKeyPair() {
-		if (!confirm('Are you sure you want to generate new keys?')) {
-			return;
-		}
-
-		const keyPair = get_keypair();
-		const publicKey = keyPair.get_public_key();
-		const privateKey = keyPair.get_private_key();
-		fetch('/api/users/update-public-key', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				email: data.session?.user?.email,
-				public_key: publicKey
-			})
-		}).then((res) => {
-			if (res.ok) {
-				download('noname-keys.txt', publicKey + '\n' + privateKey);
-			}
-		});
-	}
 </script>
 
 <svelte:window bind:innerWidth />
 
 <div class="download-key">
-	<button title="Generate new key pair" class="save" on:click={generateKeyPair}>
-		<i class="material-symbols-rounded">encrypted</i>Generate new key pair
+	<button title="Generate new keys" class="save" on:click={() => (isModalHidden = false)}>
+		<i class="material-symbols-rounded">encrypted</i>Generate New Keys
 	</button>
 	<div class="tooltip">
 		<i class="material-symbols-rounded">info</i>
-		<span class="tooltip-text {innerWidth <= 633 ? 'bottom' : 'right'}">
+		<span class="tooltip-text {innerWidth <= 608 ? 'bottom' : 'right'}">
 			These keys allow you to participate in secure surveys. Once they are generated, it is your
 			responsibility to keep them safe. When submitting a secure survey, you will be asked to
 			provide these keys to your browser for digital signature.
@@ -92,13 +47,13 @@
 		margin-right: 0.5em;
 	}
 
-	@media screen and (max-width: 1512px) {
+	@media screen and (max-width: 1480px) {
 		.tooltip {
 			--tooltip-width: 17em;
 		}
 	}
 
-	@media screen and (max-width: 1048px) {
+	@media screen and (max-width: 1016px) {
 		.tooltip {
 			--tooltip-width: 9.5em;
 		}
@@ -110,21 +65,21 @@
 		}
 	}
 
-	@media screen and (max-width: 633px) {
+	@media screen and (max-width: 608px) {
 		.tooltip {
-			--tooltip-width: 19em;
+			--tooltip-width: 18em;
 		}
 
 		.tooltip .tooltip-text.bottom {
 			left: unset;
 			right: -25%;
 			margin-left: 0em;
-			margin-right: -0.75em;
+			margin-right: -1.15em;
 		}
 
 		.tooltip .tooltip-text.bottom::after {
-			left: 92%;
-			margin-left: -0.75em;
+			left: 91.5%;
+			margin-left: -1.15em;
 		}
 	}
 </style>

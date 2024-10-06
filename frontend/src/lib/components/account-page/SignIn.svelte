c532dad394e553f30b843e55acb3c9de88829dd5
K4RP4T <kactop@st.amu.edu.pl>
2024-10-07 00:15:57 +0200

    NONAME-153 edited sign in page

--- frontend/src/lib/components/account-page/SignIn.svelte
+++ frontend/src/lib/components/account-page/SignIn.svelte
@@ -7,21 +7,36 @@
 	<button title="Google" class="sign-in" on:click={() => signIn('google')}
 		><i class="fa-brands fa-google"></i></button
 	>
-	<button title="Microsoft" class="sign-in" disabled><i class="fa-brands fa-microsoft"></i></button>
 	<button title="GitHub" class="sign-in" on:click={() => signIn('github')}
 		><i class="fa-brands fa-github"></i></button
 	>
+	<button title="X" class="sign-in" disabled><i class="fa-brands fa-x-twitter"></i></button>
 </div>
 <div title="Account information" class="info">
 	<div class="text">
 		Authorizing yourself will enable you to:
 		<ul>
-			<li>Create your own surveys,</li>
-			<li>Save survey drafts,</li>
-			<li>View your survey's results,</li>
 			<li>
-				Generate digital signature keys that allow you to participate in secure surveys without
-				needing to sign in each time.
+				<i class="material-symbols-rounded">chevron_right</i>Create both public and secure surveys,
+			</li>
+			<li>
+				<i class="material-symbols-rounded">chevron_right</i>Save surveys as drafts for later
+				editing,
+			</li>
+			<li>
+				<i class="material-symbols-rounded">chevron_right</i>View responses and summaries of your
+				surveys,
+			</li>
+			<li>
+				<i class="material-symbols-rounded">chevron_right</i>Share surveys' results with others,
+			</li>
+			<li>
+				<i class="material-symbols-rounded">chevron_right</i>Create and manage user groups,
+			</li>
+			<li>
+				<i class="material-symbols-rounded">chevron_right</i>
+				Generate digital signature keys that allow you to participate in secure surveys without needing
+				to sign in each time.
 			</li>
 		</ul>
 	</div>
@@ -34,6 +49,20 @@
 </div>
 
 <style>
+	ul {
+		list-style-type: none;
+		margin-top: 0.5em;
+		padding-left: 1.5em;
+	}
+
+	li {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: flex-start;
+		align-items: flex-start;
+		padding-top: 0.5em;
+	}
+
 	h1 {
 		text-align: center;
 		text-shadow: 0px 4px 4px var(--shadow-color);
@@ -60,19 +89,13 @@
 		font-size: 2em;
 	}
 
-	.info {
-		font-size: 1.25em;
-	}
-
 	@media screen and (max-width: 767px) {
 		h1 {
 			font-size: 2em;
 		}
 
-		.info {
-			font-size: 1em;
-			margin-left: 0em;
-			margin-right: 0em;
+		ul {
+			padding-left: 0.5em;
 		}
 	}
 </style>

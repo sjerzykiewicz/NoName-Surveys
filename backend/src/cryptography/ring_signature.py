ea95e3fd0234a8bf08b8c0f3f006911668eab456
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-10-21 16:20:15 +0200

    NONAME-173 using gmp instead of built-in ints

--- backend/src/cryptography/ring_signature.py
+++ backend/src/cryptography/ring_signature.py
@@ -1,5 +1,6 @@
 import Crypto.Hash.SHA3_384 as SHA384
 import Crypto.Hash.SHAKE256 as SHAKE256
+import gmpy2
 
 # cyclic group parameters
 # from RFC 5114
@@ -71,17 +72,17 @@ G_HEX = "".join(
 )
 
 
-p = int(P_HEX, 16)
-q = int(Q_HEX, 16)
-r = int(R_HEX, 16)
-g = int(G_HEX, 16)
+p = gmpy2.mpz(P_HEX, 16)
+q = gmpy2.mpz(Q_HEX, 16)
+r = gmpy2.mpz(R_HEX, 16)
+g = gmpy2.mpz(G_HEX, 16)
 
 
 # h1 : {0,1}* -> Zq/Z
 # hash to {0,1}^k, reduce mod q
 def h1(x: str):
     encoded = x.encode("utf-8")
-    return int(SHA384.new(encoded).hexdigest(), 16) % q
+    return gmpy2.f_mod(gmpy2.mpz(SHA384.new(encoded).hexdigest(), 16), q)
 
 
 # h2 : {0,1}* -> <g>
@@ -90,20 +91,20 @@ def h2(x: str):
     # hash to {0,1}^2716
     shake = SHAKE256.new()
     shake.update(encoded)
-    hashed = int(shake.read(2176).hex(), 16)
+    hashed = gmpy2.mpz(shake.read(2176).hex(), 16)
 
     # reduce mod (p-1) -> element of Z(p-1)/Z
-    hashed = hashed % (p - 1)
+    hashed = gmpy2.f_mod(hashed, (p - 1))
 
     # add 1 -> element of Z*p
-    hashed += 1
+    hashed = gmpy2.add(hashed, 1)
 
     # i^r is an element of <g>
-    return pow(hashed, r, p)
+    return gmpy2.powmod(hashed, r, p)
 
 
-def verify_lrs(message: str, keys: list[int], signature: list[int]) -> bool:
-    concatenated_keys: str = "".join([str(k) for k in keys])
+def verify_lrs(message: str, keys: list[gmpy2.mpz], signature: list[gmpy2.mpz]) -> bool:
+    concatenated_keys: str = "".join([k.digits() for k in keys])
 
     h = h2(concatenated_keys)
 
@@ -114,21 +115,21 @@ def verify_lrs(message: str, keys: list[int], signature: list[int]) -> bool:
     s = signature[1]
     c = signature[2:]
 
-    prod_y_c = 1
+    prod_y_c = gmpy2.mpz(1)
+    sum_c = gmpy2.mpz(0)
 
     for i in range(n):
-        prod_y_c *= pow(int(keys[i]), c[i], p)
+        prod_y_c = gmpy2.mul(prod_y_c, gmpy2.powmod(int(keys[i]), c[i], p))
+        sum_c = gmpy2.f_mod(gmpy2.add(sum_c, c[i]), q)
 
-    sum_c = sum(c) % q
-
-    g_to_s = pow(g, s, p)
-    h_to_s = pow(h, s, p)
+    g_to_s = gmpy2.powmod(g, s, p)
+    h_to_s = gmpy2.powmod(h, s, p)
 
     for_hashing = (
         concatenated_keys
-        + str(y0)
+        + y0.digits()
         + message
-        + str((g_to_s * prod_y_c) % p)
-        + str((h_to_s * pow(y0, sum_c, p)) % p)
+        + (gmpy2.f_mod(gmpy2.mul(g_to_s, prod_y_c), p)).digits()
+        + (gmpy2.f_mod(gmpy2.mul(h_to_s, gmpy2.powmod(y0, sum_c, p)), p)).digits()
     )
     return sum_c == h1(for_hashing)

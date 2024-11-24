c6e8e7aff4a51365d91afd673c0392ffc3532eb1
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-11-24 13:26:05 +0100

    NONAME-182 corrected algorithm name

--- frontend/src/lib/utils/getKeyMaterial.ts
+++ frontend/src/lib/utils/getKeyMaterial.ts
@@ -1,6 +1,6 @@
 export default async function getKeyMaterial(password: string) {
 	const encoder = new TextEncoder();
-	return crypto.subtle.importKey('raw', encoder.encode(password), 'PKDF2', false, [
+	return crypto.subtle.importKey('raw', encoder.encode(password), 'PBKDF2', false, [
 		'deriveBits',
 		'deriveKey'
 	]);

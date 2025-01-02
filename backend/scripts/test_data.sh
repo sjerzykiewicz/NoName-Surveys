9aaff56d4d2ff3a554d5c21c72093ddb470b43a5
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2025-01-02 13:15:35 +0100

    added more frontend e2e tests

--- backend/scripts/test_data.sh
+++ backend/scripts/test_data.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 curl -X POST -H 'Content-Type: application/json' -d '{"user_email": "test@test.com"}' http://localhost:8000/users/register
-response=$(curl -X POST -H 'Content-Type: application/json' -d '{"user_email": "test@test.com", "title":"test","survey_structure":{"questions":[{"required":false,"question":"test","question_type":"text","details":""}]},"uses_cryptographic_module":false,"ring_members":[]}' http://localhost:8000/surveys/create)
+response=$(curl -X POST -H 'Content-Type: application/json' -d '{"user_email": "test@test.com", "title":"Test","survey_structure":{"questions":[{"required":true,"question":"test_text","question_type":"text","details":""},{"required":true,"question":"test_number","question_type":"number","min_value":0,"max_value":10},{"required":true,"question":"test_single","question_type":"single","choices":["a","b","c"]},{"required":true,"question":"test_multi","question_type":"multi","choices":["a","b","c"]},{"required":true,"question":"test_binary","question_type":"binary","choices":["Yes","No"]},{"required":true,"question":"test_scale","question_type":"scale"},{"required":true,"question":"test_slider","question_type":"slider","min_value":0,"max_value":10,"precision":1}, {"required":true,"question":"test_list","question_type":"list","choices":["list_1","list_2","list_3"]},{"required":false,"question":"test_rank","question_type":"rank","choices":["rank_1","rank_2","rank_3"]}]},"uses_cryptographic_module":false,"ring_members":[]}' http://localhost:8000/surveys/create)
 survey_code=$(echo $response | jq -r '.survey_code')
-export SURVEY_CODE=$survey_code
-echo "Survey code saved to environment variable: $SURVEY_CODE"
+export SURVEY_CODE_1=$survey_code
+echo "Survey code saved to environment variable: $SURVEY_CODE_1"
+
+response=$(curl -X POST -H 'Content-Type: application/json' -d '{"user_email": "test@test.com", "title":"Test","survey_structure":{"questions":[{"required":true,"question":"test_text","question_type":"text","details":""},{"required":false,"question":"test_number","question_type":"number","min_value":0,"max_value":10},{"required":false,"question":"test_single","question_type":"single","choices":["a","b","c"]},{"required":false,"question":"test_multi","question_type":"multi","choices":["a","b","c"]},{"required":false,"question":"test_binary","question_type":"binary","choices":["Yes","No"]},{"required":false,"question":"test_scale","question_type":"scale"},{"required":false,"question":"test_slider","question_type":"slider","min_value":0,"max_value":10,"precision":1}]},"uses_cryptographic_module":false,"ring_members":[]}' http://localhost:8000/surveys/create)
+survey_code=$(echo $response | jq -r '.survey_code')
+export SURVEY_CODE_2=$survey_code
+echo "Survey code saved to environment variable: $SURVEY_CODE_2"
+
+curl -X POST -H 'Content-Type: application/json' -d '{"user_email": "test@test.com", "survey_code": "'$SURVEY_CODE_2'", "is_enabled": false}' http://localhost:8000/surveys/set-enabled
--- frontend/tests/rootpage.test.ts
+++ frontend/tests/rootpage.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-const SURVEY_CODE = process.env.SURVEY_CODE;
+const SURVEY_CODE_1 = process.env.SURVEY_CODE_1;
+const SURVEY_CODE_2 = process.env.SURVEY_CODE_2;
 
 test('disallows nondigit code', async ({ page }) => {
 	await page.goto('/');
@@ -29,10 +30,43 @@ test('warns about nonexistent survey', async ({ page }) => {
 	await expect(page.getByTitle('Error')).toBeVisible();
 });
 
-test('check existent survey', async ({ page }) => {
+test('check required questions', async ({ page }) => {
 	await page.goto('/');
-	await page.getByTitle('Enter a survey code to fill it out').fill(SURVEY_CODE as string);
+	await page.getByTitle('Enter a survey code to fill it out').fill(SURVEY_CODE_1 as string);
 	await page.getByTitle('Submit the code').click();
 
-	await expect(page.getByTitle('Survey Title')).toBeVisible();
+	await page.getByTitle('Submit survey').click();
+
+	await expect(page.getByTitle('Error').first()).toBeVisible();
+});
+
+test('check existent survey', async ({ page }) => {
+	await page.goto('/');
+	await page.getByTitle('Enter a survey code to fill it out').fill(SURVEY_CODE_1 as string);
+	await page.getByTitle('Submit the code').click();
+
+	await page.getByTitle('Enter your answer').fill('Answer');
+	await page.getByTitle('Selected value').first().fill('1');
+	await page.getByText('a', { exact: true }).first().check();
+	await page.getByText('b', { exact: true }).last().check();
+	await page.getByText('c', { exact: true }).last().check();
+	await page.getByText('No', { exact: true }).check();
+	await page.getByText('3', { exact: true }).check();
+	await page.getByTitle('Selected value').last().fill('3');
+	await page.getByTitle('Select your answer').last().selectOption({ value: 'list_2' });
+	await page.getByText('Do not answer').check();
+	await page.getByTitle('Submit survey').click();
+
+	await expect(page.getByText('Your answer has been submitted successfully')).toBeVisible();
+});
+
+test('check not active survey', async ({ page }) => {
+	await page.goto('/');
+	await page.getByTitle('Enter a survey code to fill it out').fill(SURVEY_CODE_2 as string);
+	await page.getByTitle('Submit the code').click();
+
+	await page.getByTitle('Enter your answer').fill('Answer');
+	await page.getByTitle('Submit survey').click();
+
+	await expect(page.getByText('Survey deactivated by the creator or not found')).toBeVisible();
 });
